70235b8390d4f8dbd508b9f2275edeaafac2c07e
NaibRT <naibrt@code.edu.az>
2020-02-26 15:50:27 +0400

    Agentlik.az

---
+++ js/carousel.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+   $('.featured-slider').owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    mouseDrag:true,
+    mouseDrag:true,
+    nav:false
+   });
+
+   $('.ic-slider').owlCarousel({
+      items:1,
+      loop:true,
+      margin:10,
+      mouseDrag:true,
+      mouseDrag:true,
+      nav:false
+     });
+
+
+   $('.scr-slider').owlCarousel({
+      items:1,
+      loop:true,
+      margin:10,
+      mouseDrag:true,
+      mouseDrag:true,
+      nav:false
+     });
+   
+});
